7792c93e25411c5d7fa3804435e8da847da7f45b
Lingling Zhang <Lingling.Zhang@telus.com>
2020-05-30 13:52:21 -0400

    File creation

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -1,5 +1,8 @@
 package bootwildfly;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +12,17 @@ public class HelloWildFlyController {
 
     @RequestMapping("hello")
     public String sayHello(){
+    	try {
+    	      File myObj = new File("filename.txt");
+    	      if (myObj.createNewFile()) {
+    	        System.out.println("File created: " + myObj.getName());
+    	      } else {
+    	        System.out.println("File already exists.");
+    	      }
+    	    } catch (IOException e) {
+    	      System.out.println("An error occurred.");
+    	      e.printStackTrace();
+    	    }
         return ("Hello, SpringBoot on Wildfly");
     }
 }
