fa9f6955aac77540da77be1208e7bfcb59690ea4
Lingling Zhang <Lingling.Zhang@telus.com>
2020-05-31 05:38:11 -0400

    Dynamic path

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -3,17 +3,19 @@ package bootwildfly;
 import java.io.File;
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWildFlyController {
-
+	@Value("${path}")
+	private String path;
 
     @RequestMapping("hello")
     public String sayHello(){
     	try {
-    	      File myObj = new File("/bootwildfly2/download/filename.txt");
+    	      File myObj = new File(path + "/filename.txt");
     	      if (myObj.createNewFile()) {
     	        System.out.println("File created: " + myObj.getName());
     	      } else {
